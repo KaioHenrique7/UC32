273346497161b791d2f7d88ea07f05fd0d862ee5
Kaio <rdparadiscord@gmail.com>
2026-09-23 11:02:22 -0300

    feat: configura aplicacao express

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import session from "express-session";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET || "uc32_secret",
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+export default app;
